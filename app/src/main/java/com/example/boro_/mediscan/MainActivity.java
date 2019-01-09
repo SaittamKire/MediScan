8dd61af0394f5f7d37799532c852ead7eebcebf7
Kristian Room Jansson <krm17002@student.mdh.se>
2019-01-09 17:50:33 +0100

    Search mfield working with toast, Added APIHandler so we wont need to duplicate Work, use that to make API calls. Changed som buttonwork, can make manual searches now

--- app/src/main/java/com/example/boro_/mediscan/MainActivity.java
+++ app/src/main/java/com/example/boro_/mediscan/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.boro_.mediscan;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
@@ -12,10 +13,27 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
     private ListView mListView;
+    public ApiHandler mApiHandler;
 
     ArrayList<String> listDataHeader;
     HashMap<String, List<String>> listHash;
@@ -44,10 +63,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mApiHandler = new ApiHandler();
 
         initData();
-
-
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
@@ -70,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
                                 selectedFragment = new Scan();
                                 break;
                             case R.id.retrived_data_tab:
-                                selectedFragment = new RetrievedData(); selectedFragment.setArguments(bundle);
+                                selectedFragment = new RetrievedData();
+                                selectedFragment.setArguments(bundle);
                                 break;
                         }
                         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -86,12 +105,15 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
 
 
-        //Dialog logic
+        //Dialog logic (NEEDS TO BE UPDATED TO LOOK UP IF THE USER HAS ACCEPTED THIS MESSAGE ALREADY)
         MyDialogClass cdd = new MyDialogClass(this); //Creates disclaimer-dialog
         cdd.show(); //Shows it
         cdd.setCanceledOnTouchOutside(false); //Disables cancelations
         cdd.setCancelable(false);
         //End dialog logic
+
+        setupButtons(); //Will setup topbar buttons and searchfield
+
     }
 
 
@@ -131,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
         listDataHeader.add("Organisationsinfo");
         listDataHeader.add("Förpackning");
 
-        List <String> Produkt = new ArrayList<>();
+        List<String> Produkt = new ArrayList<>();
         Produkt.add("Treo");
         Produkt.add("Receptfritt");
         Produkt.add("Läkemedel");
@@ -140,16 +162,16 @@ public class MainActivity extends AppCompatActivity {
         Produkt.add("Ej narkotikaklassad");
         Produkt.add("N");
 
-        List <String> Compounds = new ArrayList<>();
+        List<String> Compounds = new ArrayList<>();
         Compounds.add("Acetylsalicylsyra\nKoffein");
 
-        List <String> OrgInfo = new ArrayList<>();
+        List<String> OrgInfo = new ArrayList<>();
         OrgInfo.add("Treo AB");
         OrgInfo.add("Treogatan 33");
         OrgInfo.add("Treolian Empire");
         OrgInfo.add("Innehavare av godkännande för försäljning");
 
-        List <String> Package = new ArrayList<>();
+        List<String> Package = new ArrayList<>();
         Package.add("Rör, 10 brustabletter");
         Package.add("2 år");
         Package.add("Ingen särskild temperaturbegränsning");
@@ -161,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     public void initData2() {
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
@@ -171,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
         listDataHeader.add("Organisationsinfo");
         listDataHeader.add("Förpackning");
 
-        List <String> Produkt = new ArrayList<>();
+        List<String> Produkt = new ArrayList<>();
         Produkt.add("Alvedon");
         Produkt.add("Receptfritt");
         Produkt.add("Läkemedel");
@@ -180,16 +203,16 @@ public class MainActivity extends AppCompatActivity {
         Produkt.add("Ej narkotikaklassad");
         Produkt.add("N");
 
-        List <String> Compounds = new ArrayList<>();
+        List<String> Compounds = new ArrayList<>();
         Compounds.add("Paracetamol");
 
-        List <String> OrgInfo = new ArrayList<>();
+        List<String> OrgInfo = new ArrayList<>();
         OrgInfo.add("Alvedon AB");
         OrgInfo.add("Alvedonvägen 56");
         OrgInfo.add("Sverige");
         OrgInfo.add("Innehavare av godkännande för försäljning");
 
-        List <String> Package = new ArrayList<>();
+        List<String> Package = new ArrayList<>();
         Package.add("Burk, 2000 tabletter");
         Package.add("20 år");
         Package.add("Förvaras med fördel i ugn");
@@ -202,10 +225,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void updateView(List<String> lh, HashMap<String, List<String>> lhm)
-    {
+    public void updateView(List<String> lh, HashMap<String, List<String>> lhm) {
         lh = listDataHeader;
         lhm = listHash;
     }
 
+    private void setupButtons() {
+
+        final EditText SearchBar = (EditText) findViewById(R.id.search_bar);
+        SearchBar.setOnEditorActionListener( //Will setup Search bar logic, when user clicks okay a api call will be made!
+                new EditText.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        String SearchValue = null;
+
+                        if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                                actionId == EditorInfo.IME_ACTION_DONE ||
+                                event != null &&
+                                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                            if (event == null || !event.isShiftPressed()) {
+                                // the user is done typing.
+                                SearchValue = SearchBar.getText().toString();
+                                ApiFirstSearchNoStrengthCallback(SearchValue, "sv"); //Callback to get Context to ApiHandler
+                                return true; // consume.
+                            }
+                        }
+                        return false; // pass on to other listeners.
+                    }
+                }
+        );
+
+        //ADD MORE BUTTON LOGIC
+
+    }
+
+    public void ApiFirstSearchNoStrengthCallback(String name, String language) { //Need callback to get Context. REMOVE LANGUAGE IN AND MAKE APP-LANGUAGE SPECIFIC INSTEAD!
+        mApiHandler.FirstSearchNoStrength(name, language, this);
+    }
 }
+
+
